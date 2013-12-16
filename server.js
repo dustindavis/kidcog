135b364f287622180f51e303532db23f56774482
unknown <dustindavis@mac-ssaifee.mpi.local>
2013-12-16 11:40:23 -0800

    Output index.html

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 80;
 
 http.createServer(
 function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello, World!');
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	fs.readFile('index.html', function(err, data) {
+		res.end(data);
+	});
 }
 
 ).listen(port);
