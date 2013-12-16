d83fe9c3118ab769de7f2ae2fbeaa2ad7dbbffbe
unknown <dustindavis@mac-ssaifee.mpi.local>
2013-12-16 11:32:34 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var port = process.env.PORT || 80;
+
+http.createServer(
+function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello, World!');
+}
+
+).listen(port);
