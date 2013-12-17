cd1444fb1e537e29929336955399a5b185b80e97
unknown <dustindavis@mac-ssaifee.mpi.local>
2013-12-17 10:01:27 -0800

    added animations

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 80;
+var url = require('url');
 
 http.createServer(
 function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	fs.readFile('index.html', function(err, data) {
-		res.end(data);
-	});
+	var pathname = url.parse(req.url).pathname;
+	
+	if(pathname.indexOf('.png') > 0) {
+	console.log(pathname);
+		res.writeHead(200, { 'Content-Type': 'image/png' });
+		
+		fs.readFile('.' + pathname, function(err, data) {
+			res.end(data);
+		});
+	
+	} else {
+
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		
+		fs.readFile('index.html', function(err, data) {
+			res.end(data);
+		});
+	}
 }
 
 ).listen(port);
